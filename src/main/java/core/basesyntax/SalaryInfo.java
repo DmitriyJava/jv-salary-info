f69fbc26f235656832a1f827e018aa1827606dc0
user <dkmc80@gmail.com>
2026-04-19 21:14:43 +0300

    fixed solution task salary-info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,38 +1,39 @@
 package core.basesyntax;
 
-public class SalaryInfo {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int from = convertDateToInt(dateFrom);
-        int to = convertDateToInt(dateTo);
+public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INITIAL_SALARY = 0;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
+
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
         result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
 
         for (String name : names) {
-            int totalSalary = 0;
+            int totalSalary = INITIAL_SALARY;
 
             for (String entry : data) {
                 String[] parts = entry.split(" ");
-                int currentDate = convertDateToInt(parts[0]);
-                String currentName = parts[1];
-                int hours = Integer.parseInt(parts[2]);
-                int salary = Integer.parseInt(parts[3]);
-                if (currentName.equals(name) && currentDate >= from && currentDate <= to) {
+                LocalDate currentDate = LocalDate.parse(parts[DATE_INDEX], FORMATTER);
+                String currentName = parts[NAME_INDEX];
+                int hours = Integer.parseInt(parts[HOURS_INDEX]);
+                int salary = Integer.parseInt(parts[INCOME_INDEX]);
+                if (currentName.equals(name) && !currentDate.isBefore(from) && !currentDate
+                        .isAfter(to)) {
                     totalSalary += hours * salary;
                 }
             }
-            result.append(System.lineSeparator())
-                    .append(name)
-                    .append(" - ")
-                    .append(totalSalary);
+            result.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
         }
-
         return result.toString();
     }
-
-    int convertDateToInt(String date) {
-        String[] parts = date.split("\\.");
-        String sortedDate = parts[2] + parts[1] + parts[0];
-        return Integer.parseInt(sortedDate);
-    }
 }
+
